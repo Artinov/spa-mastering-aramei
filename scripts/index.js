d36bbb8c320281b145cad426903f83d8e57dd9d7
Artur <ara@mail.com>
2017-04-24 16:20:08 +0300

    completed home work 17

--- scripts/index.js
+++ scripts/index.js
@@ -10,12 +10,12 @@ $(document).ready(function() {
         if (pageName.length == 0) {
             $("#test > div").hide();
             return;
-        }
+        };
 
         if (page.pages.indexOf(pageName) != -1) {
             if (pageName == "" || pageName == page.active) {
                 return;
-            }
+            };
 
             $("#test > div:not([data-page='" + pageName + "'])").hide();
             $("#test > div[data-page='" + pageName + "']").show();
@@ -31,14 +31,43 @@ $(document).ready(function() {
 
                 if (pageName == "" || pageName == page.active) {
                     return;
-                }
+                };
 
                 $("#test > div:not([data-page='" + pageName + "'])").hide();
 
-
                 page.active = pageName;
             });
-        }
+        };
 
     });
 });
+
+function changePage(pageName) {
+    if (page.pages.indexOf(pageName) != -1) {
+        if (pageName == "" || pageName == page.active) {
+            return;
+        };
+
+        $("#test > div:not([data-page='" + pageName + "'])").hide();
+        $("#test > div[data-page='" + pageName + "']").show();
+
+        page.active = pageName;
+    } else {
+        page.pages.push(pageName);
+
+        $.get("./pages/" + pageName + ".html").then(function(res) {
+            var wrapperDiv = document.createElement("div");
+            $(wrapperDiv).attr("data-page", pageName).html(res);
+            $("#test").append(wrapperDiv);
+
+            if (pageName == "" || pageName == page.active) {
+                return;
+            };
+
+            $("#test > div:not([data-page='" + pageName + "'])").hide();
+
+            page.active = pageName;
+        });
+    };
+};
+
